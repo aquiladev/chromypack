46f94ee4cdecc1be2c81f43ac76c88205e5805e9
Sergii Bomko <aquila.ua@gmail.com>
2016-01-02 01:06:56 +0200

    init

---
+++ chromypack.Test/DriverPackageManagerTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace chromypack.Test
+{
+	public class DriverPackageManagerTest
+	{
+		private readonly DriverPackageManager _manager;
+
+		public DriverPackageManagerTest()
+		{
+			_manager = new DriverPackageManager();
+		}
+
+		[Fact]
+		public void TestMethod1()
+		{
+			_manager.BuildPackage();
+		}
+	}
+}
---
+++ chromypack/VersionLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace chromypack
+{
+	public class VersionLoader
+	{
+		public Task<byte[]> DownloadAsync(string version)
+		{
+			using (var client = new HttpClient())
+			{
+				var url = $"http://chromedriver.storage.googleapis.com/{version}/chromedriver_win32.zip";
+				return client.GetAsync(url)
+					.ContinueWith(request =>
+					{
+						HttpResponseMessage response = request.Result;
+						response.EnsureSuccessStatusCode();
+						return response.Content.ReadAsByteArrayAsync();
+					}).Result;
+			}
+		}
+	}
+}
